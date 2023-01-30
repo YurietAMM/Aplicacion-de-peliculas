edaf603b0e8bb2f7eb41ea8ac2b7c645d2ff5cd1
Yuriet Mosquera <yurietalejandromosquera@gmail.com>
2023-01-30 11:30:06 -0500

    first commit

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,9 +9,14 @@ import { Router } from '@angular/router';
 })
 export class CrearGeneroComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  constructor(private router : Router, private formBuilder: FormBuilder) { }
+
+  formulario!: FormGroup;
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nombre: ''
+    });
   }
 
   guardar() {
